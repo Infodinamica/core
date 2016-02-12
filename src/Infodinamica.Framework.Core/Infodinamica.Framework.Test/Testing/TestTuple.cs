4e1811ce62ca2cc7206104d2a7f413da591c614a
Infodinamica <contacto@infodinamica.cl>
2016-02-12 13:14:33 -0300

    Primera version
    Se sube la primera version que contiene la clase Tuple para .net 3.5

---
+++ src/Infodinamica.Framework.Core/Infodinamica.Framework.Test/Testing/TestTuple.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Infodinamica.Framework.Core.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Infodinamica.Framework.Test.Testing
+{
+    /// <summary>
+    /// Descripción resumida de TestTuple
+    /// </summary>
+    [TestClass]
+    public class TestTuple
+    {
+
+        [TestMethod]
+        public void CreateTupleByProperties()
+        {
+            var date = DateTime.Now;
+            var number = 10;
+            var text = "hello!";
+
+            var tuple = new Tuple<string, DateTime, int>();
+            tuple.Item3 = number;
+            tuple.Item2 = date;
+            tuple.Item1 = text;
+
+            if (tuple.Item3 != number || tuple.Item2 != date || tuple.Item1 != text)
+                throw new Exception("Tuple lost values");
+        }
+
+        [TestMethod]
+        public void CreateTupleByConstructor()
+        {
+            var date = DateTime.Now;
+            var number = 10;
+            var text = "hello!";
+
+            var tuple = new Tuple<string, DateTime, int>(text, date, number);
+            
+            if (tuple.Item3 != number || tuple.Item2 != date || tuple.Item1 != text)
+                throw new Exception("Tuple lost values");
+        }
+    }
+}

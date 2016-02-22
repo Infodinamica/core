4c89d12bccf269bac4b51b1557926a3923eefd29
Infodinamica <contacto@infodinamica.cl>
2016-02-21 21:31:24 -0300

    Tuple hasta nueve y extensiones para propertyInfo y string
    Tuple hasta T9.
    IsNumeric, IsDate, IsBoolean, IsNullable como extensiones para
    propertyInfo
    IsNullOrWhiteSpace para string

---
+++ src/Infodinamica.Framework.Core/Infodinamica.Framework.Core/Extensions/Reflection/PropertyInfoMethods.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+
+namespace Infodinamica.Framework.Core.Extensions.Reflection
+{
+    public static class TypeMethods
+    {
+        public static bool IsNumeric(this PropertyInfo prop)
+        {
+            if (
+                prop.PropertyType == typeof(sbyte) ||
+                prop.PropertyType == typeof(byte) ||
+                prop.PropertyType == typeof(short) ||
+                prop.PropertyType == typeof(ushort) ||
+                prop.PropertyType == typeof(int) ||
+                prop.PropertyType == typeof(uint) ||
+                prop.PropertyType == typeof(long) ||
+                prop.PropertyType == typeof(ulong) ||
+                prop.PropertyType == typeof(double) ||
+                prop.PropertyType == typeof(decimal) ||
+                prop.PropertyType == typeof(SByte) ||
+                prop.PropertyType == typeof(Byte) ||
+                prop.PropertyType == typeof(Int16) ||
+                prop.PropertyType == typeof(Int32) ||
+                prop.PropertyType == typeof(Int64) ||
+                prop.PropertyType == typeof(UInt16) ||
+                prop.PropertyType == typeof(UInt32) ||
+                prop.PropertyType == typeof(UInt64) ||
+                prop.PropertyType == typeof(Double) ||
+                prop.PropertyType == typeof(Decimal) 
+                )
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsDate(this PropertyInfo prop)
+        {
+            return prop.PropertyType == typeof (DateTime);
+        }
+
+        public static bool IsTime(this PropertyInfo prop)
+        {
+            return prop.PropertyType == typeof (TimeSpan);
+        }
+
+        public static bool IsDateOrTime(this PropertyInfo prop)
+        {
+            return prop.IsDate() || prop.IsTime();
+        }
+
+        public static bool IsBoolean(this PropertyInfo prop)
+        {
+            return prop.PropertyType == typeof (Boolean) || prop.PropertyType == typeof (bool);
+        }
+
+        public static bool IsNullable(this PropertyInfo prop)
+        {
+            return !prop.PropertyType.IsValueType || prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof (Nullable<>);
+        }
+    }
+}

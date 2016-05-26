491a8e84074812403f4a16d7adcde5b2ed1f1873
Infodinamica <contacto@infodinamica.cl>
2016-05-26 15:59:59 -0400

    Dictionary Extension Method
    Se agrega un método de extensión para diccionarios, para convertirlos en
    clases planas PlainItem

---
+++ src/Infodinamica.Framework.Core/Infodinamica.Framework.Core/Containers/PlainItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infodinamica.Framework.Core.Containers
+{
+    /// <summary>
+    /// Representa la información de un diccionario llave-valor
+    /// </summary>
+    public class PlainItem
+    {
+        /// <summary>
+        /// Valor de la opción
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Texto de la opción
+        /// </summary>
+        public string Text { get; set; }
+    }
+}
---
+++ src/Infodinamica.Framework.Core/Infodinamica.Framework.Core/Extensions/Dictionary/DictionaryMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infodinamica.Framework.Core.Containers;
+
+namespace Infodinamica.Framework.Core.Extensions.Dictionary
+{
+    /// <summary>
+    /// Extensiones para diccionarios
+    /// </summary>
+    public static class DictionaryMethods
+    {
+        /// <summary>
+        /// Transforma un diccionario en un listado de PlainItem
+        /// </summary>
+        /// <typeparam name="T">Tipo de dato a castear</typeparam>
+        /// <param name="source">Diccionario a castear</param>
+        /// <returns>Listado de PlainItem</returns>
+        public static IList<PlainItem> ToList<T>(this IDictionary<T, string> source) where T:struct
+        {
+            return source.Select(i => new PlainItem() {Text = i.Value, Value = i.Key.ToString()}).ToList();
+        }
+    }
+}

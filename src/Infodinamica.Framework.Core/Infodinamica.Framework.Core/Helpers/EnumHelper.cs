6077dde7d74d6292470999f7ac18b649fd81abbd
Infodinamica <contacto@infodinamica.cl>
2016-05-29 17:09:19 -0400

    Enums y .NET
    Se agrega comatibilidad con .NET Framework 4.0
    Se agrega un helper que permite obtener los display attribute desde los
    enum

---
+++ src/Infodinamica.Framework.Core/Infodinamica.Framework.Core/Helpers/EnumHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+#if(NET40)
+using System.ComponentModel.DataAnnotations;
+#endif
+
+namespace Infodinamica.Framework.Core.Helpers
+{
+    /// <summary>
+    /// Contiene utilidades para trabajar con enums
+    /// </summary>
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Obtiene los valores y nombres del enum
+        /// </summary>
+        /// <typeparam name="T">Enum a leer</typeparam>
+        /// <returns>Diccionario en donde el campo key corresponde al valor del enum, mientras que el campo value corresponde al nombre del Enum</returns>
+        public static IDictionary<int, string> GetValuesAndNames<T>()
+        {
+            var elements = new Dictionary<int, string>();
+            foreach (var element in Enum.GetValues(typeof(T)))
+            {
+                elements.Add((int)element, element.ToString());
+            }
+            return elements;
+        }
+
+#if(NET40)
+        /// <summary>
+        /// Obtiene los valores y DisplayAttribute configurado en el Enum. En caso de no estar configurado el attribute, muestra el nombre del item del Enum
+        /// </summary>
+        /// <typeparam name="T">Enum a leer</typeparam>
+        /// <returns>Diccionario en donde el campo key corresponde al valor del enum, mientras que el campo value corresponde al nombre del enum</returns>
+        public static IDictionary<int, string> GetValuesAndDisplayNames<T>()
+        {
+            var elements = new Dictionary<int, string>();
+            foreach (var element in Enum.GetValues(typeof(T)))
+            {
+                var name = GetDisplayValue(element);
+                if (string.IsNullOrWhiteSpace(name))
+                    name = element.ToString();
+                elements.Add((int)element, name);
+            }
+            return elements;
+        }
+
+        /// <summary>
+        /// Obtiene el DisplayAttribute de un elemento dado de un Enum
+        /// </summary>
+        /// <typeparam name="T">Tipo del Enum a leer</typeparam>
+        /// <param name="value">Item del enum a leer</param>
+        /// <returns>Retorna el DisplayAttribute configurado (en caso de existir) o retorna un string vacío en caso de no estarlo</returns>
+        public static string GetDisplayValue<T>(T value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var descriptionAttributes = fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), false) as DisplayAttribute[];
+
+            if (descriptionAttributes == null) return string.Empty;
+            return (descriptionAttributes.Length > 0) ? descriptionAttributes[0].Name : value.ToString();
+        }
+#endif
+
+    }
+}
--- src/Infodinamica.Framework.Core/Infodinamica.Framework.Core/Properties/AssemblyInfo.cs
+++ src/Infodinamica.Framework.Core/Infodinamica.Framework.Core/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.*")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.*")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
